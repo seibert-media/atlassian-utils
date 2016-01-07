d68bea63ac9d32d48e46877d86444d4145c5928a
Benjamin Borbe <bborbe@rocketnews.de>
2016-01-07 19:19:23 +0100

    add bitbucket scripts

---
+++ bitbucket/bitbucket.go
@@ -0,0 +1,8 @@
+package bitbucket
+
+const (
+	JSON_URL     = "https://my.atlassian.com/download/feeds/current/stash.json"
+	ARCH         = "all"
+	PACKAGE_NAME = "bitbucket"
+	TARGET       = "/opt/bitbucket"
+)
